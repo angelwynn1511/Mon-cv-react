ed06fb44b42afa68a66be668131e2c3c541620a5
angelique_renard <angelique_renard@penguin>
2022-09-20 23:03:25 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Parcours from "./pages/Parcours";
+import Portfolio from "./pages/Portfolio";
+import Error from "./pages/Error";
+import Presentation from "./pages/Presentation";
+import Contact from "./pages/Contact";
+import SharedLayout from "./pages/SharedLayout";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+
+            {/* J'insère dans ma route Home mes routes About et error */}
+            <Route path="home" element={<Home />} />
+            <Route path="parcours" element={<Parcours />} />
+            <Route path="presentation" element={<Presentation />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+          {/* Tous les liens qui ne seront pas ceux du dessus enclencheront une erreur */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
